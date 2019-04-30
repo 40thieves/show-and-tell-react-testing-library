8b7fd4e9e5ae517c075fcc4685b2a05669a4b3dc
Alasdair Smith <ali@alasdairsmith.co.uk>
2019-04-30 17:28:25 +0100

    Clean up theme

--- theme.js
+++ theme.js
@@ -1,32 +1,18 @@
-import React from 'react'
-import { okaidia as syntaxTheme } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { default as theme, syntaxHighlighterPrism } from '@mdx-deck/themes'
+import { okaidia as syntaxTheme } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
-import { Prism } from 'react-syntax-highlighter'
-import merge from 'lodash.merge'
-
-import { getLanguage } from '@mdx-deck/themes/syntax-highlighter'
-
-export const pre = props => (
-  <div style={{ fontSize: '1.4rem' }}>{props.children}</div>
-)
-
-export const createCode = (opts = {}) => props => {
-  const language = getLanguage(props.className)
-  return <Prism {...opts} language={language} style={syntaxTheme} {...props} />
+export default {
+  ...theme,
+  ...syntaxHighlighterPrism({ prism: { style: syntaxTheme } }),
+  colors: {
+    code: '#000'
+  },
+  pre: {
+    fontSize: '1.3rem'
+  },
+  Slide: {
+    width: '80vw',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  }
 }
-
-export default theme =>
-  merge(theme, {
-    colors: {
-      code: '#000'
-    },
-    Slide: {
-      width: '80vw',
-      marginLeft: 'auto',
-      marginRight: 'auto'
-    },
-    components: {
-      pre,
-      code: createCode(theme.prism),
-    },
-  })
